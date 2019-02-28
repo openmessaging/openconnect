1f4d500b0921fadc865ed2dc6c43ed3d13d78232
ymwneu <ymwneu@126.com>
2019-02-28 15:46:41 +0800

    Optimize the comments.

--- connector/src/main/java/io/openmessaging/connector/api/Connector.java
+++ connector/src/main/java/io/openmessaging/connector/api/Connector.java
@@ -1,8 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.openmessaging.connector.api;
 
 import io.openmessaging.KeyValue;
 import java.util.List;
 
+/**
+ * Connector has the responsibility of manage the data integration between message queue and other system.
+ */
 public interface Connector {
 
     /**
@@ -34,12 +54,14 @@ public interface Connector {
 
     /**
      * Returns the Task implementation for this Connector.
+     *
      * @return
      */
     Class<? extends Task> taskClass();
 
     /**
      * Returns a set of configurations for Tasks based on the current configuration.
+     *
      * @return
      */
     List<KeyValue> taskConfigs();
